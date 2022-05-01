b0968d8cd62cfe9f5ce4079a4c97f5c2ddbe2c1c
mohammad13133 <mohammad-bashar-01@hotmail.com>
2022-05-02 00:37:55 +0300

    edit admin jframe name

--- WeatherForecastAPP/src/adminInterface.java
+++ WeatherForecastAPP/src/adminInterface.java
@@ -7,12 +7,12 @@
  *
  * @author mohammad
  */
-public class admin extends javax.swing.JFrame {
+public class adminInterface extends javax.swing.JFrame {
 
     /**
      * Creates new form admin
      */
-    public admin() {
+    public adminInterface() {
         initComponents();
     }
 
@@ -85,20 +85,21 @@ public class admin extends javax.swing.JFrame {
                 }
             }
         } catch (ClassNotFoundException ex) {
-            java.util.logging.Logger.getLogger(admin.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(adminInterface.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (InstantiationException ex) {
-            java.util.logging.Logger.getLogger(admin.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(adminInterface.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (IllegalAccessException ex) {
-            java.util.logging.Logger.getLogger(admin.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(adminInterface.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (javax.swing.UnsupportedLookAndFeelException ex) {
-            java.util.logging.Logger.getLogger(admin.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(adminInterface.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         }
+        //</editor-fold>
         //</editor-fold>
 
         /* Create and display the form */
         java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
-                new admin().setVisible(true);
+                new adminInterface().setVisible(true);
             }
         });
     }
