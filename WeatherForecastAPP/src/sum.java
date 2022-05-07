83ee7c55e7e77976cfa119c338d793eb5c0bfeed
mohammad13133 <mohammad-bashar-01@hotmail.com>
2022-05-07 21:35:00 +0300

    add junit

---
+++ WeatherForecastAPP/src/sum.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author mohammad
+ */
+public class sum {
+    int add(int x,int y){
+        
+        return x+y;
+    }
+}
